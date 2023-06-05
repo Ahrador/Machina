4f551ce0019a2faceb8fca51f28a6099644bb069
Alen Poparic <alenpopari@gmail.com>
2023-06-05 21:17:38 +0200

    Predavanje 7, parts

---
+++ Predavanja/7 - Headeri/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// Predavanje 7 - Switch, Function overloading, headeri, STL string
+
+/*
+Timestamps:
+13:22 - Podjela klasa radi modularnosti (header za deklaracije), (.cpp za definicije)
+14:16 - Overloading 2
+14:21 - Pauza 15 min
+14:37 - Stringovi
+*/
+
+
+// Headeri
+
+int main() {
+
+
+
+
+
+}
