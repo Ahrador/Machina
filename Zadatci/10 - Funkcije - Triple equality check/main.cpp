35a974e57c80805d5c16dc7bb56f42434567af6d
Alen Poparic <alenpopari@gmail.com>
2023-06-06 13:03:13 +0200

    Triple equality check, 2nd commit

--- Zadatci/10 - Funkcije - Triple equality check/main.cpp
+++ Zadatci/10 - Funkcije - Triple equality check/main.cpp
@@ -8,11 +8,10 @@ Napiši funkciju koja prima tri argumenta i vraća true ako su svi isti,
 a false ako nisu.
 */
 
-
 // Funkcija koja provjerava jesu li sva tri argumenta ista
 bool triple_kill(int a, int b, int c) {  // bool jer je true/false
 	if (a == b && b == c) {
-		cout << true;
+		return true; // popravljeno
 	}
 	else {
 		return false;
@@ -24,6 +23,7 @@ int main() {
 	int prvi_argument = 10;
 	int drugi_argument = 3;
 	int treci_argument = 8;
+
 	if (triple_kill(prvi_argument, drugi_argument, treci_argument)) {
 		cout << "Svi brojevi su isti" << endl;
 	}
